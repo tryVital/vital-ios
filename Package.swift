29e915229a625551aebe3c71c26a7c798bcb5dd3
Rui Peres <sauron.is.alive@gmail.com>
2022-06-07 10:11:46 +0100

    Different approach

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "vital-ios",
     platforms: [
-      .iOS(.v14),
-      .macOS(.v11)
+      .iOS(.v14)
     ],
     products: [
         .library(
