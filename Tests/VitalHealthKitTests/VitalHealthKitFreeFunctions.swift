4a0c82a32fc305e76d47d63bfa0d439fb18ae193
Rui Peres <sauron.is.alive@gmail.com>
2022-11-04 18:45:05 +0000

    test realistic payload

--- Tests/VitalHealthKitTests/VitalHealthKitFreeFunctions.swift
+++ Tests/VitalHealthKitTests/VitalHealthKitFreeFunctions.swift
@@ -72,6 +72,65 @@ class VitalHealthKitFreeFunctionsTests: XCTestCase {
     let array2 = accumulate([s4, s8, s3, s6, s7, s9, s5, s2, s1], calendar: calendar)
     assert(array: array2)
   }
+
+  
+  func testAccumulateRealisticExample() {
+    let calendar = Calendar.current
+
+    func makeSample(hour: Int, minute: Int, value: Double) -> QuantitySample {
+      let date = calendar.date(bySettingHour: hour, minute: minute, second: 0, of: Date())!
+      return .init(value: value, date: date, sourceBundle: "1", unit: "")
+    }
+    
+    let array = [
+    makeSample(hour: 7, minute: 3, value: 1),
+    makeSample(hour: 7, minute: 25, value: 57),
+    makeSample(hour: 7, minute: 27, value: 48),
+    makeSample(hour: 7, minute: 35, value: 19),
+    makeSample(hour: 8, minute: 5, value: 189),
+    makeSample(hour: 8, minute: 14, value: 254),
+    makeSample(hour: 8, minute: 18, value: 126),
+    makeSample(hour: 8, minute: 28, value: 124),
+    makeSample(hour: 9, minute: 22, value: 11),
+    
+    makeSample(hour: 9, minute: 44, value: 56),
+    makeSample(hour: 10, minute: 33, value: 66),
+    makeSample(hour: 13, minute: 35, value: 67),
+    makeSample(hour: 14, minute: 09, value: 32),
+    makeSample(hour: 14, minute: 10, value: 23),
+    makeSample(hour: 14, minute: 17, value: 19),
+    makeSample(hour: 14, minute: 19, value: 39),
+    makeSample(hour: 14, minute: 43, value: 8),
+    makeSample(hour: 15, minute: 02, value: 55),
+    
+    makeSample(hour: 15, minute: 28, value: 5),
+    makeSample(hour: 16, minute: 19, value: 168),
+    makeSample(hour: 16, minute: 31, value: 320),
+    makeSample(hour: 16, minute: 39, value: 90),
+    makeSample(hour: 16, minute: 46, value: 325),
+    makeSample(hour: 16, minute: 46, value: 68),
+    makeSample(hour: 16, minute: 49, value: 132),
+    makeSample(hour: 16, minute: 59, value: 130),
+    makeSample(hour: 17, minute: 07, value: 201),
+    
+    makeSample(hour: 17, minute: 48, value: 114),
+    makeSample(hour: 17, minute: 52, value: 114),
+    makeSample(hour: 18, minute: 7, value: 101),
+    makeSample(hour: 18, minute: 17, value: 245),
+    makeSample(hour: 18, minute: 26, value: 244),
+    makeSample(hour: 18, minute: 31, value: 70),
+    makeSample(hour: 18, minute: 51, value: 120),
+    makeSample(hour: 19, minute: 05, value: 8),
+    makeSample(hour: 19, minute: 34, value: 141),
+    makeSample(hour: 20, minute: 50, value: 68),
+    makeSample(hour: 21, minute: 08, value: 35),
+    ]
+    
+    let array1 = accumulate(array, calendar: calendar)
+    let array2 = accumulate(array1, calendar: calendar)
+
+    XCTAssert(array1 == array2)
+  }
   
   func testAverageTwoElements() {
     let calendar = Calendar.current
