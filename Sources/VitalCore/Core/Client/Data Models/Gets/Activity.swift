8b2e1ec7da8438ee8c5c4e185505791d5fc1ef47
Rui Peres <sauron.is.alive@gmail.com>
2022-11-18 16:21:53 +0000

    Add summary

---
+++ Sources/VitalCore/Core/Client/Data Models/Gets/Activity.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct ActivityResponse: Equatable, Decodable {
+  public var activity: [ActivitySummary]
+}
+
+public struct ActivitySummary: Equatable, Decodable {
+  public var id: UUID
+  public var date: Date
+  public var caloriesTotal: Double?
+  public var caloriesActive: Double?
+  public var steps: Int?
+  public var dailyMovement: Int?
+  public var low: Double?
+  public var medium: Double?
+  public var high: Double?
+  public var source: SourceSummary
+  public var floorsClimbed: Int?
+  public var timezoneOffset: Int?
+}
--- Sources/VitalCore/Core/Client/Endpoints/VitalClient+Summary.swift
+++ Sources/VitalCore/Core/Client/Endpoints/VitalClient+Summary.swift
@@ -73,4 +73,21 @@ public extension VitalClient.Summary {
     
     return result.value.sleep
   }
+  
+  func activitySummary(
+    startDate: Date,
+    endDate: Date? = nil
+  ) async throws -> [ActivitySummary] {
+    let userId = await self.client.userId.get()
+    let configuration = await self.client.configuration.get()
+    
+    let prefix: String = "/\(configuration.apiVersion)/\(self.resource)/"
+    let fullPath = prefix + "activity/\(userId)"
+    let query = makeBaseDatesQuery(startDate: startDate, endDate: endDate)
+    
+    let request: Request<ActivityResponse> = .get(fullPath, query: query)
+    let result = try await configuration.apiClient.send(request)
+    
+    return result.value.activity
+  }
 }
