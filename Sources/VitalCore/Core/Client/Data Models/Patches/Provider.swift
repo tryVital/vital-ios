90c4bf838f666f5052cfdd9bd2e559a6ba90dde9
Anders Ha <anders@tryvital.io>
2023-03-18 16:50:49 +0000

    Support exhaustive checks

--- Sources/VitalCore/Core/Client/Data Models/Patches/Provider.swift
+++ Sources/VitalCore/Core/Client/Data Models/Patches/Provider.swift
@@ -13,42 +13,97 @@ public struct Provider: Equatable {
   public let slug: Slug
   public let logo: String
 
-  public struct Slug: Hashable, RawRepresentable, Codable, ExpressibleByStringLiteral {
-    public static let beurerBLE: Self = "beurer_ble"
-    public static let beurer: Self = "beurer_api"
-    public static let omronBLE: Self = "omron_ble"
-    public static let accuchekBLE: Self = "accuchek_ble"
-    public static let contourBLE: Self = "contour_ble"
-    public static let appleHealthKit: Self = "apple_health_kit"
-    public static let manual: Self = "manual"
-    public static let iHealth: Self = "ihealth"
-    public static let libreBLE: Self = "freestyle_libre_ble"
-    public static let libre: Self = "freestyle_libre"
-    public static let oura: Self = "oura"
-    public static let garmin: Self = "garmin"
-    public static let fitbit: Self = "fitbit"
-    public static let whoop: Self = "whoop"
-    public static let strava: Self = "strava"
-    public static let renpho: Self = "renpho"
-    public static let peloton: Self = "peloton"
-    public static let wahoo: Self = "wahoo"
-    public static let zwift: Self = "zwift"
-    public static let eightSleep: Self = "eight_sleep"
-    public static let withings: Self = "withings"
-    public static let googleFit: Self = "google_fit"
-    public static let hammerhead: Self = "hammerhead"
-    public static let dexcom: Self = "dexcom"
-    public static let myFitnessPal: Self = "my_fitness_pal"
-    public static let healthConnect: Self = "health_connect"
+  public enum Slug: Hashable, Codable, RawRepresentable {
+    case beurerBLE
+    case beurer
+    case omronBLE
+    case accuchekBLE
+    case contourBLE
+    case appleHealthKit
+    case manual
+    case iHealth
+    case libreBLE
+    case libre
+    case oura
+    case garmin
+    case fitbit
+    case whoop
+    case strava
+    case renpho
+    case peloton
+    case wahoo
+    case zwift
+    case eightSleep
+    case withings
+    case googleFit
+    case hammerhead
+    case dexcom
+    case myFitnessPal
+    case healthConnect
+    case unknown(String)
 
-    public let rawValue: String
-
-    public init(stringLiteral value: StringLiteralType) {
-      self.init(rawValue: value)
+    public var rawValue: String {
+      switch self {
+      case .beurerBLE: return "beurer_ble"
+      case .beurer: return "beurer_api"
+      case .omronBLE: return "omron_ble"
+      case .accuchekBLE: return "accuchek_ble"
+      case .contourBLE: return "contour_ble"
+      case .appleHealthKit: return "apple_health_kit"
+      case .manual: return "manual"
+      case .iHealth: return "ihealth"
+      case .libreBLE: return "freestyle_libre_ble"
+      case .libre: return "freestyle_libre"
+      case .oura: return "oura"
+      case .garmin: return "garmin"
+      case .fitbit: return "fitbit"
+      case .whoop: return "whoop"
+      case .strava: return "strava"
+      case .renpho: return "renpho"
+      case .peloton: return "peloton"
+      case .wahoo: return "wahoo"
+      case .zwift: return "zwift"
+      case .eightSleep: return "eight_sleep"
+      case .withings: return "withings"
+      case .googleFit: return "google_fit"
+      case .hammerhead: return "hammerhead"
+      case .dexcom: return "dexcom"
+      case .myFitnessPal: return "my_fitness_pal"
+      case .healthConnect: return "health_connect"
+      case let .unknown(rawValue): return rawValue
+      }
     }
 
-    public init(rawValue: String) {
-      self.rawValue = rawValue
+    public init?(rawValue: String) {
+      switch rawValue {
+      case "beurer_ble": self = .beurerBLE
+      case "beurer_api": self = .beurer
+      case "omron_ble": self = .omronBLE
+      case "accuchek_ble": self = .accuchekBLE
+      case "contour_ble": self = .contourBLE
+      case "apple_health_kit": self = .appleHealthKit
+      case "manual": self = .manual
+      case "ihealth": self = .iHealth
+      case "freestyle_libre_ble": self = .libreBLE
+      case "freestyle_libre": self = .libre
+      case "oura": self = .oura
+      case "garmin": self = .garmin
+      case "fitbit": self = .fitbit
+      case "whoop": self = .whoop
+      case "strava": self = .strava
+      case "renpho": self = .renpho
+      case "peloton": self = .peloton
+      case "wahoo": self = .wahoo
+      case "zwift": self = .zwift
+      case "eight_sleep": self = .eightSleep
+      case "withings": self = .withings
+      case "google_fit": self = .googleFit
+      case "hammerhead": self = .hammerhead
+      case "dexcom": self = .dexcom
+      case "my_fitness_pal": self = .myFitnessPal
+      case "health_connect": self = .healthConnect
+      default: self = .unknown(rawValue)
+      }
     }
   }
 }
--- Tests/VitalCoreTests/ProviderTests.swift
+++ Tests/VitalCoreTests/ProviderTests.swift
@@ -11,9 +11,26 @@ class ProviderTests: XCTestCase {
 
     XCTAssertEqual(data, "\"\(slug.rawValue)\"".data(using: .utf8))
   }
+
   func test_provider_slug_decoding() throws {
     let decoder = JSONDecoder()
     let slug = Provider.Slug.appleHealthKit
+    let data = try XCTUnwrap("\"\(slug.rawValue)\"".data(using: .utf8))
+
+    XCTAssertEqual(slug, try decoder.decode(Provider.Slug.self, from: data))
+  }
+
+  func test_provider_slug_encoding_unknown_value() throws {
+    let encoder = JSONEncoder()
+    let slug = Provider.Slug.unknown("whomst")
+    let data = try encoder.encode(slug)
+
+    XCTAssertEqual(data, "\"\(slug.rawValue)\"".data(using: .utf8))
+  }
+
+  func test_provider_slug_decoding_unknown_value() throws {
+    let decoder = JSONDecoder()
+    let slug = Provider.Slug.unknown("whomst")
     let data = try XCTUnwrap("\"\(slug.rawValue)\"".data(using: .utf8))
 
     XCTAssertEqual(slug, try decoder.decode(Provider.Slug.self, from: data))
