0f49838a6a020ccdb406095dcf14d062731200a1
Anders Ha <anders@tryvital.io>
2023-04-25 11:33:31 +0100

    Support BP BLE devices with RACP unimplemented

--- Sources/VitalDevices/Extensions/AnyPublisher+Utils.swift
+++ Sources/VitalDevices/Extensions/AnyPublisher+Utils.swift
@@ -4,7 +4,9 @@ extension AnyPublisher {
   static var empty: AnyPublisher {
     Empty().eraseToAnyPublisher()
   }
-  
+}
+
+extension Publisher {
   func flatMapLatest<P: Publisher>(_ f: @escaping (Self.Output) -> P) -> AnyPublisher<P.Output, P.Failure> where P.Failure == Self.Failure {
     map(f).switchToLatest().eraseToAnyPublisher()
   }
