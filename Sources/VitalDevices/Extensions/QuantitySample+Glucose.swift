bd59c5a5d6ddeeec08efe6bfc6cb87f1da283c54
Rui Peres <sauron.is.alive@gmail.com>
2023-08-16 10:25:12 +0100

    Expose the raw glucose

--- Sources/VitalDevices/Extensions/QuantitySample+Glucose.swift
+++ Sources/VitalDevices/Extensions/QuantitySample+Glucose.swift
@@ -1,4 +1,42 @@
 import VitalCore
+
+/// We use this approach so that the JSON payload for `QuantitySample`
+/// looks nicer:
+///
+/// ```
+/// {
+///  "metadata": {
+///   "glucose": {
+///   ...
+///   }
+///  }
+/// }
+private struct FreestyleLibreGlucoseDigest: Encodable {
+  struct Digest: Encodable {
+    let rawValue: Int
+    let rawTemperature: Int
+    let temperatureAdjustment: Int
+    let hasError: Bool
+    let dataQuality: Glucose.DataQuality
+    let dataQualityFlags: Int
+
+    init(glucose: Glucose) {
+      self.rawValue = glucose.rawValue
+      self.rawTemperature = glucose.rawTemperature
+      self.temperatureAdjustment = glucose.temperatureAdjustment
+      self.hasError = glucose.hasError
+      self.dataQuality = glucose.dataQuality
+      self.dataQualityFlags = glucose.dataQualityFlags
+    }
+  }
+
+  let glucose: Digest
+
+  init(glucose: Glucose) {
+    self.glucose = Digest(glucose: glucose)
+  }
+}
+
 extension QuantitySample {
   init(glucose: Glucose) {
     self.init(
@@ -7,7 +45,9 @@ extension QuantitySample {
       startDate: glucose.date,
       endDate: glucose.date,
       type: "automatic",
-      unit: "mmol/L"
+      unit: "mmol/L",
+      metadata: FreestyleLibreGlucoseDigest(glucose: glucose)
+        .eraseToAnyEncodable()
     )
   }
 }
