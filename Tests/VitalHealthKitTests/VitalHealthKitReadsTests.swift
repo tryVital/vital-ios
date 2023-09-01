9c7c31b0f59e42c9ef4241b3dd837de73a4533c9
Anders Ha <anders@tryvital.io>
2023-09-01 20:03:48 +0100

    VIT-4277: Improve HealthKit sleep stitching for iPhone samples

--- Tests/VitalHealthKitTests/VitalHealthKitReadsTests.swift
+++ Tests/VitalHealthKitTests/VitalHealthKitReadsTests.swift
@@ -82,14 +82,14 @@ class VitalHealthKitReadsTests: XCTestCase {
       productType: "product"
     )
     
-    let sleeps = stichedSleeps(sleeps: [sleep, sleep1])
+    let sleeps = stitchedSleeps(sleeps: [sleep, sleep1], sourceType: .watch)
     
     XCTAssert(sleeps.count == 1)
     XCTAssert(sleeps[0].startDate == Date("2022-08-06 21:35:31"))
     XCTAssert(sleeps[0].endDate == Date("2022-08-07 05:32:00"))
     
     
-    let sleeps1 = stichedSleeps(sleeps: [sleep1, sleep])
+    let sleeps1 = stitchedSleeps(sleeps: [sleep1, sleep], sourceType: .watch)
     
     XCTAssert(sleeps1.count == 1)
     XCTAssert(sleeps1[0].startDate == Date("2022-08-06 21:35:31"))
@@ -102,7 +102,7 @@ class VitalHealthKitReadsTests: XCTestCase {
       productType: "product"
     )
     
-    let sleeps2 = stichedSleeps(sleeps: [sleep1, sleep2])
+    let sleeps2 = stitchedSleeps(sleeps: [sleep1, sleep2], sourceType: .watch)
     
     XCTAssert(sleeps2.count == 1)
     XCTAssert(sleeps2[0].startDate == Date("2022-08-06 23:45:00"))
@@ -131,19 +131,19 @@ class VitalHealthKitReadsTests: XCTestCase {
       productType: "product"
     )
     
-    let sleeps = stichedSleeps(sleeps: [sleep, sleep1, sleep2])
+    let sleeps = stitchedSleeps(sleeps: [sleep, sleep1, sleep2], sourceType: .watch)
     
     XCTAssert(sleeps.count == 1)
     XCTAssert(sleeps[0].startDate == Date("2022-08-07 00:07:00"))
     XCTAssert(sleeps[0].endDate == Date("2022-08-07 07:43:00"))
     
-    let sleeps1 = stichedSleeps(sleeps: [sleep2, sleep1, sleep])
+    let sleeps1 = stitchedSleeps(sleeps: [sleep2, sleep1, sleep], sourceType: .watch)
     
     XCTAssert(sleeps1.count == 1)
     XCTAssert(sleeps1[0].startDate == Date("2022-08-07 00:07:00"))
     XCTAssert(sleeps1[0].endDate == Date("2022-08-07 07:43:00"))
     
-    let sleeps2 = stichedSleeps(sleeps: [sleep1, sleep, sleep2])
+    let sleeps2 = stitchedSleeps(sleeps: [sleep1, sleep, sleep2], sourceType: .watch)
     
     XCTAssert(sleeps2.count == 1)
     XCTAssert(sleeps2[0].startDate == Date("2022-08-07 00:07:00"))
@@ -166,18 +166,50 @@ class VitalHealthKitReadsTests: XCTestCase {
       productType: "product"
     )
     
-    let sleeps = stichedSleeps(sleeps: [sleep, sleep1])
+    let sleeps = stitchedSleeps(sleeps: [sleep, sleep1], sourceType: .watch)
 
     XCTAssert(sleeps.count == 1)
     XCTAssert(sleeps[0].startDate == Date("2022-08-06 21:35:31"))
     XCTAssert(sleeps[0].endDate == Date("2022-08-07 05:33:00"))
     
     
-    let sleeps1 = stichedSleeps(sleeps: [sleep1, sleep])
+    let sleeps1 = stitchedSleeps(sleeps: [sleep1, sleep], sourceType: .watch)
     
     XCTAssert(sleeps1.count == 1)
     XCTAssert(sleeps1[0].startDate == Date("2022-08-06 21:35:31"))
     XCTAssert(sleeps1[0].endDate == Date("2022-08-07 05:33:00"))
+  }
+
+  func testStichSleep_no_overlap_iphoneSamplesWithLongGaps() {
+
+    let sleep = SleepPatch.Sleep(
+      startDate: Date("2022-08-06 21:35:31"),
+      endDate: Date("2022-08-07 05:30:00"),
+      sourceBundle: "com.apple.health",
+      productType: "product"
+    )
+
+    // Awake for 91 minutes, and then nap for 24 minutes
+
+    let sleep1 = SleepPatch.Sleep(
+      startDate: Date("2022-08-07 07:01:00"),
+      endDate: Date("2022-08-07 07:25:00"),
+      sourceBundle: "com.apple.health",
+      productType: "product"
+    )
+
+    let sleeps = stitchedSleeps(sleeps: [sleep, sleep1], sourceType: .phone)
+
+    XCTAssert(sleeps.count == 1)
+    XCTAssert(sleeps[0].startDate == Date("2022-08-06 21:35:31"))
+    XCTAssert(sleeps[0].endDate == Date("2022-08-07 07:25:00"))
+
+
+    let sleeps1 = stitchedSleeps(sleeps: [sleep1, sleep], sourceType: .phone)
+
+    XCTAssert(sleeps1.count == 1)
+    XCTAssert(sleeps1[0].startDate == Date("2022-08-06 21:35:31"))
+    XCTAssert(sleeps1[0].endDate == Date("2022-08-07 07:25:00"))
   }
   
   func testStatisticalReadingForLegacyUser() async throws {
