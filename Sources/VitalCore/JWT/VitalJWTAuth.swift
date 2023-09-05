566de0701cf1bcb5f7159b7c29b6b4c0f59cfeb3
Anders Ha <anders@tryvital.io>
2023-09-05 17:10:10 +0100

    VIT-4321: VitalJWTAuth sign-in flow

--- Sources/VitalCore/JWT/VitalJWTAuth.swift
+++ Sources/VitalCore/JWT/VitalJWTAuth.swift
@@ -24,8 +24,50 @@ internal actor VitalJWTAuth {
     self.session = URLSession(configuration: .ephemeral)
   }
 
-  func signIn() async throws {
-    // TODO: Perform the token exchange API call
+  func signIn(_ token: String) async throws {
+    guard try getRecord() == nil else {
+      // Already signed-in
+      return
+    }
+
+    let signInToken = try VitalSignInToken.decode(from: token)
+    let claims = try signInToken.unverifiedClaims()
+
+    var components = URLComponents(string: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken")!
+    components.queryItems = [URLQueryItem(name: "key", value: signInToken.publicKey)]
+    var request = URLRequest(url: components.url!)
+
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = try JSONEncoder().encode(
+      FirebaseTokenExchangeRequest(token: signInToken.userToken, tenantId: claims.teamId)
+    )
+
+    let (data, response) = try await session.data(for: request)
+    let httpResponse = response as! HTTPURLResponse
+
+    switch httpResponse.statusCode {
+    case 200 ... 299:
+      let decoder = JSONDecoder()
+      let exchangeResponse = try decoder.decode(FirebaseTokenExchangeResponse.self, from: data)
+
+      let record = VitalJWTAuthRecord(
+        userId: claims.userId,
+        teamId: claims.teamId,
+        publicApiKey: signInToken.publicKey,
+        accessToken: exchangeResponse.idToken,
+        refreshToken: exchangeResponse.refreshToken,
+        expiry: Date().addingTimeInterval(Double(exchangeResponse.expiresIn) ?? 3600)
+     )
+
+      try setRecord(record)
+
+    case 401:
+      throw VitalJWTAuthError.reauthenticationNeeded
+
+    default:
+      throw VitalJWTAuthError.tokenRefreshFailure
+    }
   }
 
   func signOut() async throws {
@@ -148,7 +190,7 @@ private struct VitalJWTAuthRecord: Codable {
   }
 }
 
-private struct FirebaseTokenRefreshResponse: Codable {
+private struct FirebaseTokenRefreshResponse: Decodable {
   let expiresIn: String
   let refreshToken: String
   let idToken: String
@@ -157,6 +199,84 @@ private struct FirebaseTokenRefreshResponse: Codable {
     case expiresIn = "expires_in"
     case refreshToken = "refresh_token"
     case idToken = "id_token"
+  }
+}
+
+private struct FirebaseTokenExchangeRequest: Encodable {
+  let returnSecureToken = true
+  let token: String
+  let tenantId: String
+}
+
+private struct FirebaseTokenExchangeResponse: Decodable {
+  let expiresIn: String
+  let refreshToken: String
+  let idToken: String
+}
+
+internal struct VitalSignInToken: Hashable, Decodable {
+  let publicKey: String
+  let userToken: String
+
+  init(publicKey: String, userToken: String) {
+    self.publicKey = publicKey
+    self.userToken = userToken
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case publicKey = "public_key"
+    case userToken = "user_token"
+  }
+
+  static func decode(from token: String) throws -> VitalSignInToken {
+    guard let unwrappedData = Data(base64Encoded: token) else {
+      throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "token is not valid base64 blob"))
+    }
+    return try JSONDecoder().decode(VitalSignInToken.self, from: unwrappedData)
+  }
+
+  func unverifiedClaims() throws -> VitalSignInTokenClaims {
+    let components = userToken.split(separator: ".")
+    guard components.count == 3 else {
+      throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "malformed JWT [0]"))
+    }
+
+    guard
+      let rawHeader = Data(base64Encoded: padBase64(components[0])),
+      let rawClaims = Data(base64Encoded: padBase64(components[1]))
+    else {
+      throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "malformed JWT [1]"))
+    }
+
+    let decoder = JSONDecoder()
+    let headers = try decoder.decode([String: String].self, from: rawHeader)
+
+    guard headers["alg"] == "RS256" && headers["typ"] == "JWT" else {
+      throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "malformed JWT [2]"))
+    }
+
+    return try decoder.decode(VitalSignInTokenClaims.self, from: rawClaims)
+  }
+}
+
+private func padBase64(_ string: any StringProtocol) -> String {
+  let pad = string.count % 4
+  print(pad)
+  print(string)
+  if pad == 0 {
+    return String(string)
+  } else {
+    return string + String(repeating: "=", count: 4 - pad)
+  }
+}
+
+internal struct VitalSignInTokenClaims: Decodable {
+  let userId: String
+  let teamId: String
+
+  enum CodingKeys: String, CodingKey {
+    case userId = "uid"
+    case teamId = "tenant_id"
   }
 }
 
