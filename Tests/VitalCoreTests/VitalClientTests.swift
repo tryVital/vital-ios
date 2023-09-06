ff925367880a5d92142cfe7e21d6c6c4147ea605
Anders Ha <anders@tryvital.io>
2023-09-06 19:27:19 +0100

    VIT-4343: Infer SDK environment & region from the Vital Sign-In Token

--- Tests/VitalCoreTests/VitalClientTests.swift
+++ Tests/VitalCoreTests/VitalClientTests.swift
@@ -31,8 +31,7 @@ class VitalClientTests: XCTestCase {
     /// I don't feel particularly happy with this approach. The only reason I know that I should
     /// call `setConfiguration` is because I know the implementation, which sort of defeats the point.
     client.setConfiguration(
-      apiKey: apiKey,
-      environment: environment,
+      strategy: .apiKey(apiKey, environment),
       configuration: .init(logsEnable: false),
       storage: storage,
       apiVersion: apiVersion,
@@ -41,13 +40,12 @@ class VitalClientTests: XCTestCase {
     
     await VitalClient.setUserId(UUID(uuidString: userId)!)
     
-    let securePayload: VitalCoreSecurePayload? = try? secureStorage.get(key: core_secureStorageKey)
+    let securePayload: VitalClientRestorationState? = try? secureStorage.get(key: core_secureStorageKey)
     let storedUserId: String? = try? secureStorage.get(key: user_secureStorageKey)
     
     XCTAssertEqual(securePayload?.configuration.logsEnable, false)
     XCTAssertEqual(securePayload?.apiVersion, apiVersion)
-    XCTAssertEqual(securePayload?.apiKey, apiKey)
-    XCTAssertEqual(securePayload?.environment, environment)
+    XCTAssertEqual(securePayload?.strategy, ConfigurationStrategy.apiKey(apiKey, environment))
     
     XCTAssertEqual(storedUserId, userId)
     XCTAssertTrue(
@@ -59,7 +57,7 @@ class VitalClientTests: XCTestCase {
     XCTAssertTrue(VitalClient.shared.apiKeyModeUserId.isNil())
     XCTAssertTrue(VitalClient.shared.configuration.isNil())
     
-    let nilSecurePayload: VitalCoreSecurePayload? = try? secureStorage.get(key: core_secureStorageKey)
+    let nilSecurePayload: VitalClientRestorationState? = try? secureStorage.get(key: core_secureStorageKey)
     let nilStoredUserId: UUID? = try? secureStorage.get(key: user_secureStorageKey)
     
     XCTAssertNil(nilSecurePayload)
@@ -70,7 +68,7 @@ class VitalClientTests: XCTestCase {
     )
   }
 
-  func testAutoUserIdConfiguration() async {
+  func testAutomaticConfiguration_autoMigrateFromLegacyAPIMode() async throws {
     let _: Void = await withCheckedContinuation { continuation in
       VitalClient.automaticConfiguration {
         continuation.resume(returning: ())
@@ -83,11 +81,15 @@ class VitalClientTests: XCTestCase {
     XCTAssertTrue(nilUserId)
     XCTAssertTrue(nilConfiguration)
 
-    let securePayload = VitalCoreSecurePayload(
+    let securePayload = VitalClientRestorationState(
       configuration: .init(),
       apiVersion: apiVersion,
+
+      // Payload stored by Legacy API Mode in an earlier release contains `apiKey` and `environment`
+      // but not `strategy`.
       apiKey: apiKey,
-      environment: environment
+      environment: environment,
+      strategy: nil
     )
 
     let secureStorage = VitalSecureStorage(keychain: .debug)
@@ -103,11 +105,58 @@ class VitalClientTests: XCTestCase {
       }
     }
 
-    let nonNilConfiguration = VitalClient.shared.configuration.isNil()
-    let nonNilUserId = VitalClient.shared.apiKeyModeUserId.isNil()
+    // TEST: Configuration is set
+    let configuration = try XCTUnwrap(VitalClient.shared.configuration.value)
 
-    XCTAssertFalse(nonNilUserId)
-    XCTAssertFalse(nonNilConfiguration)
+    // TEST: Auth Mode is API Key
+    XCTAssertEqual(configuration.authMode, .apiKey)
+
+    // TEST: API Key Mode User ID equals to fake user ID
+    XCTAssertEqual(VitalClient.shared.apiKeyModeUserId.value?.uuidString, userId)
+  }
+
+  func testAutomaticConfiguration_userJWTMode() async throws {
+    let _: Void = await withCheckedContinuation { continuation in
+      VitalClient.automaticConfiguration {
+        continuation.resume(returning: ())
+      }
+    }
+
+    let nilConfiguration = VitalClient.shared.configuration.isNil()
+    let nilUserId = VitalClient.shared.apiKeyModeUserId.isNil()
+
+    XCTAssertTrue(nilUserId)
+    XCTAssertTrue(nilConfiguration)
+
+    let securePayload = VitalClientRestorationState(
+      configuration: .init(),
+      apiVersion: apiVersion,
+      apiKey: nil,
+      environment: nil,
+      strategy: .jwt(environment)
+    )
+
+    let secureStorage = VitalSecureStorage(keychain: .debug)
+    try! secureStorage.set(value: userId, key: user_secureStorageKey)
+    try! secureStorage.set(value: securePayload, key: core_secureStorageKey)
+
+    let newClient = VitalClient(secureStorage: secureStorage)
+    VitalClient.setClient(newClient)
+
+    let _: Void = await withCheckedContinuation { continuation in
+      VitalClient.automaticConfiguration {
+        continuation.resume(returning: ())
+      }
+    }
+
+    // TEST: Configuration is set
+    let configuration = try XCTUnwrap(VitalClient.shared.configuration.value)
+
+    // TEST: Auth Mode is JWT
+    XCTAssertEqual(configuration.authMode, .userJwt)
+
+    // TEST: API Key Mode User ID is nil
+    XCTAssertNil(VitalClient.shared.apiKeyModeUserId.value)
   }
 
   func testAutoConfigurationDoesNotFailWithUserIdWithoutConfiguration() async {
@@ -143,8 +192,7 @@ class VitalClientTests: XCTestCase {
     storage.storeConnectedSource(for: userId, with: provider)
     
     client.setConfiguration(
-      apiKey: apiKey,
-      environment: environment,
+      strategy: .apiKey(apiKey, environment),
       configuration: .init(logsEnable: false),
       storage: storage,
       apiVersion: apiVersion,
@@ -164,8 +212,7 @@ class VitalClientTests: XCTestCase {
     storage.storeConnectedSource(for: userId, with: provider)
 
     client.setConfiguration(
-      apiKey: apiKey,
-      environment: environment,
+      strategy: .apiKey(apiKey, environment),
       configuration: .init(logsEnable: false),
       storage: storage,
       apiVersion: apiVersion,
