75fc44ea61aa88d04011a9a67ef48546f1b3caf0
Anders Ha <anders@tryvital.io>
2024-06-21 12:50:44 +0100

    VIT-6711: Fix SDK timeseries API typing (#150)

--- Sources/VitalCore/Core/Client/Data Models/Base Models/TimeSeriesDataPoint.swift
+++ Sources/VitalCore/Core/Client/Data Models/Base Models/TimeSeriesDataPoint.swift
@@ -6,12 +6,14 @@ public struct ScalarSample: Equatable, Decodable {
   public let value: Float
   public let type: String?
   public let unit: String
+  public let timezoneOffset: Int?
 
-  public init(timestamp: Date, value: Float, type: String?, unit: String) {
+  public init(timestamp: Date, value: Float, type: String?, unit: String, timezoneOffset: Int?) {
     self.timestamp = timestamp
     self.value = value
     self.type = type
     self.unit = unit
+    self.timezoneOffset = timezoneOffset
   }
 }
 
@@ -22,23 +24,46 @@ public struct IntervalSample: Equatable, Decodable, Hashable {
   public let value: Float
   public let type: String?
   public let unit: String
+  public let timezoneOffset: Int?
 
-  public init(start: Date, end: Date, value: Float, type: String?, unit: String) {
+  public init(start: Date, end: Date, value: Float, type: String?, unit: String, timezoneOffset: Int?) {
     self.start = start
     self.end = end
     self.value = value
     self.type = type
     self.unit = unit
+    self.timezoneOffset = timezoneOffset
   }
 }
 
-public struct GroupedSamplesResponse<Sample: Equatable & Decodable>: Equatable, Decodable {
-  public let groups: [GroupedSamples<Sample>]
-  public let next: String?
+public struct BloodPressureSample: Equatable, Decodable, Hashable {
+  public let timestamp: Date
 
-  public init(groups: [GroupedSamples<Sample>], next: String?) {
+  public let systolic: Float
+  public let diastolic: Float
+
+  public let type: String?
+  public let unit: String
+  public let timezoneOffset: Int?
+
+  public init(timestamp: Date, systolic: Float, diastolic: Float, type: String?, unit: String, timezoneOffset: Int?) {
+    self.timestamp = timestamp
+    self.systolic = systolic
+    self.diastolic = diastolic
+    self.type = type
+    self.unit = unit
+    self.timezoneOffset = timezoneOffset
+  }
+}
+
+
+public struct GroupedSamplesResponse<Sample: Equatable & Decodable>: Equatable, Decodable {
+  public let groups: [Provider.Slug: [GroupedSamples<Sample>]]
+  public let nextCursor: String?
+
+  public init(groups: [Provider.Slug: [GroupedSamples<Sample>]], nextCursor: String?) {
     self.groups = groups
-    self.next = next
+    self.nextCursor = nextCursor
   }
 }
 
