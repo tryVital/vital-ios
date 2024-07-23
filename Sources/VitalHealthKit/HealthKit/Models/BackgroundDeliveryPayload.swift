6d29787e3523cd8a6eebfe1fc7fb542073bbf01d
Anders Ha <anders@tryvital.io>
2024-07-23 20:37:43 +0100

    VIT-6992: Protect foreground sync work with UIKit background task

--- Sources/VitalHealthKit/HealthKit/Models/BackgroundDeliveryPayload.swift
+++ Sources/VitalHealthKit/HealthKit/Models/BackgroundDeliveryPayload.swift
@@ -1,8 +1,10 @@
 import HealthKit
+import UIKit
 
 struct BackgroundDeliveryPayload {
   let sampleTypes: Set<HKSampleType>
   let completion: (Completion) -> Void
+  let appState: UIApplication.State
 
   enum Completion {
     case cancelled
