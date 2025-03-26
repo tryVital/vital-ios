61c352010476f5f7526d29df6eb452c035c521df
Anders Ha <anders@tryvital.io>
2025-03-26 12:07:10 +0000

    Release 1.4.6

--- Sources/VitalCore/Core/Client/VitalClient.swift
+++ Sources/VitalCore/Core/Client/VitalClient.swift
@@ -134,7 +134,7 @@ public enum AuthenticateRequest {
 }
 
 @objc public class VitalClient: NSObject {
-  public static let sdkVersion = "1.4.5"
+  public static let sdkVersion = "1.4.6"
 
   @_spi(VitalSDKInternals)
   public let configuration: ProtectedBox<VitalCoreConfiguration>
