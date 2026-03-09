ef2d9c86e8af7163397ea9d63e239fdecceec2d1
Anders Ha <anders@junction.com>
2026-03-09 19:19:41 +0000

    Release 1.8.5

--- Sources/VitalCore/Core/Client/VitalClient.swift
+++ Sources/VitalCore/Core/Client/VitalClient.swift
@@ -153,7 +153,7 @@ public enum AuthenticateRequest {
 }
 
 @objc public class VitalClient: NSObject {
-  public static let sdkVersion = "1.8.4"
+  public static let sdkVersion = "1.8.5"
 
   @_spi(VitalSDKInternals)
   public let configuration: ProtectedBox<VitalCoreConfiguration>
